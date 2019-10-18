b0ea5316749072c0fcc1e3a5387a5a1dffd8d913
Mahmoud Elshakoushy <mahmoudelshakoushy@mahmouds-mac.local>
2019-10-18 14:26:19 -0700

    Initial Commit

---
+++ iTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTalk
+//
+//  Created by Mahmoud Elshakoushy on 10/18/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
