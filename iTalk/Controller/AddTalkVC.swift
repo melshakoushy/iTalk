1d1100fadd0a9b2c6cc82f6cdc353275e4502a0e
Mahmoud Elshakoushy <mahmoudelshakoushy@Mahmouds-Mac.local>
2019-10-19 08:49:43 -0700

    first commit

---
+++ iTalk/Controller/AddTalkVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddTalkVC.swift
+//  iTalk
+//
+//  Created by Mahmoud Elshakoushy on 10/19/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class AddTalkVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iTalk/Utilities/Enums.swift
@@ -0,0 +1,9 @@
+//
+//  Enums.swift
+//  iTalk
+//
+//  Created by Mahmoud Elshakoushy on 10/19/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import Foundation
